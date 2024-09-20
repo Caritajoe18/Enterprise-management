e9074c0a5d1968bc66978a41f28ca8d5dd6abad7
Carita Ndibe <carita@Caritas-MacBook-Air.local>
2024-09-20 13:26:39 +0100

    parsing json in products

---
+++ src/migrations/20240920121952-create-products.ts
@@ -0,0 +1,40 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.createTable('Products', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      price: {
+        type: Sequelize.JSON,
+        allowNull: false,
+      },
+      pricePlan: {
+        type: Sequelize.JSON,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
+    await queryInterface.dropTable('Products');
+  },
+};
