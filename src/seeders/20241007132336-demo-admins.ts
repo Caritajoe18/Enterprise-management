27efee35af4c4e31ad7f5e37255c8875dc7480b1
Carita Ndibe <carita@Caritas-MacBook-Air.local>
2024-10-07 15:20:42 +0100

    resolved Customers Issue

--- src/seeders/20241007132336-demo-admins.ts
+++ src/seeders/20241007132336-demo-admins.ts
@@ -1,30 +1,30 @@
-import { QueryInterface, DataTypes } from "sequelize";
-import { v4 as uuidv4 } from "uuid";
+// import { QueryInterface, DataTypes } from "sequelize";
+// import { v4 as uuidv4 } from "uuid";
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    await queryInterface.bulkInsert(
-      "Admins",
-      [
-        {
-          id: uuidv4(),
-          firstname: "admin",
-          lastname: "admin",
-          email: "admin@example.com",
-          phoneNumber: +23480,
-          password: "Pass12345",
-          //confirmPassword:"Pass12345",
-          isAdmin: true,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+//     await queryInterface.bulkInsert(
+//       "Admins",
+//       [
+//         {
+//           id: uuidv4(),
+//           firstname: "admin",
+//           lastname: "admin",
+//           email: "admin@example.com",
+//           phoneNumber: +23480,
+//           password: "Pass12345",
+//           //confirmPassword:"Pass12345",
+//           isAdmin: true,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//       ],
+//       {}
+//     );
+//   },
 
-  async down(queryInterface: QueryInterface) {
-    await queryInterface.bulkDelete("NavParents", {}, {});
-  },
-};
+//   async down(queryInterface: QueryInterface) {
+//     await queryInterface.bulkDelete("NavParents", {}, {});
+//   },
+// };
