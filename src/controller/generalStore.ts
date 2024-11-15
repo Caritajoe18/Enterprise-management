a256cf2d7bfe7bc4ccc4634d3490e5988eea70c0
Carita Ndibe <carita@Caritas-MacBook-Air.local>
2024-11-15 05:46:08 +0100

    notify

--- src/controller/generalStore.ts
+++ src/controller/generalStore.ts
@@ -6,11 +6,15 @@ import {
 } from "../validations/productValidations";
 import GeneralStore from "../models/generalStore";
 import GeneralOrder from "../models/generalOrders";
-import { addQuantityToStore, removeQuantityFromStore } from "../utilities/modules";
+import {
+  addQuantityToStore,
+  removeQuantityFromStore,
+} from "../utilities/modules";
+import Products from "../models/products";
 
 export const createGenStore = async (req: Request, res: Response) => {
   try {
-    const { name } = req.body;
+    const { name, productId, departmentId } = req.body;
     const { error, value } = genStoreValidationSchema.validate(req.body, {
       abortEarly: false,
     });
@@ -18,6 +22,14 @@ export const createGenStore = async (req: Request, res: Response) => {
     if (error) {
       const errors = error.details.map((detail) => detail.message);
       return res.status(400).json({ errors });
+    }
+    const product = await Products.findOne({
+      where: { id: productId, departmentId },
+    });
+    if (!product) {
+      return res.status(404).json({
+        message: `Product not found in department or does not exist`,
+      });
     }
 
     const exist = await GeneralStore.findOne({ where: { name } });
@@ -101,9 +113,18 @@ export const editGenStore = async (req: Request, res: Response) => {
 export const createGenOrder = async (req: Request, res: Response) => {
   try {
     const { orders } = req.body;
-
+    if (!Array.isArray(orders) || orders.length === 0) {
+      return res.status(400).json({ message: "Orders array cannot be empty." });
+    }
+    const productIds = new Set();
     const validatedOrders = [];
     for (const order of orders) {
+      if (productIds.has(order.productId)) {
+        return res.status(400).json({
+          message: `Duplicate order for product ID ${order.productId} is not allowed.`,
+        });
+      }
+      productIds.add(order.productId);
       const { error, value } = genOrderValidationSchema.validate(order, {
         abortEarly: false,
       });
@@ -120,7 +141,7 @@ export const createGenOrder = async (req: Request, res: Response) => {
       if (!shelf) {
         return res
           .status(404)
-          .json({ message: "Product or products not found" });
+          .json({ message: "Product or products not found in store, please add to store to place order" });
       }
       validatedOrders.push(value);
     }
@@ -190,10 +211,16 @@ export const deleteGenStore = async (req: Request, res: Response) => {
   }
 };
 
-export const addQuantityToGeneralStore = async (req: Request, res: Response) => {
+export const addQuantityToGeneralStore = async (
+  req: Request,
+  res: Response
+) => {
   return addQuantityToStore(req, res, GeneralStore);
 };
 
-export const removeQuantityFromGeneralStore = async (req: Request, res: Response) => {
+export const removeQuantityFromGeneralStore = async (
+  req: Request,
+  res: Response
+) => {
   return removeQuantityFromStore(req, res, GeneralStore);
 };
