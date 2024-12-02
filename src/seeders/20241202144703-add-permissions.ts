fe717210d8ea5403ff03db4378c54628e49810bc
Carita Ndibe <carita@Caritas-MacBook-Air.local>
2024-12-02 17:09:53 +0100

    fixed seeding permissions

--- src/seeders/20241202144703-add-permissions.ts
+++ src/seeders/20241202144703-add-permissions.ts
@@ -1,30 +1,29 @@
-// import { QueryInterface, DataTypes } from "sequelize";
-// import { v4 as uuidv4 } from "uuid";
+import { QueryInterface, DataTypes } from "sequelize";
+import { v4 as uuidv4 } from "uuid";
 
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.bulkInsert(
+      "Permissions",
+      [
+        {
+          id: uuidv4(),
+          name: "Place Order",
+          slug:"place-order",
+          navParentId: "e837484c-713a-4526-bb9e-8a8434c82828",
+          url: "raise-customer-order",
+          orderIndex: 3,
+          isNav: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//     async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-  
-//         await queryInterface.bulkInsert(
-//             "Permissions",
-//             [
-//         {
-//           id: uuidv4(),
-//           firstname: "admin",
-//           lastname: "admin",
-//           email: "admin3@example.com",
-//           phoneNumber: +234801111,
-//           isAdmin: true,
-//           createdAt: new Date(),
-//           updatedAt: new Date(),
-//         },
-//       ],
-//       {}
-//     );
-//   },
-
-//   async down(queryInterface: QueryInterface) {
-//     await queryInterface.bulkDelete("Permissions", {}, {});
-//   },
-// };
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.bulkDelete("Permissions", {}, {});
+  },
+};
